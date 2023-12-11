0bf8c390d1de711961a0e98708e93f2b1854c8ae
Liz <lfalconriv@gmail.com>
2023-12-11 14:07:08 -0300

    splash screen

--- Parada88/Assets/UI Toolkit/Pannels/MainMenuPresenter.cs
+++ Parada88/Assets/UI Toolkit/Pannels/MainMenuPresenter.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuPresenter : MonoBehaviour
 {
-    void Awake()
+	
+    void Start()
     {
 		var root = GetComponent<UIDocument>().rootVisualElement;
+	
 		
 		root.Q<Button>("Start").clicked += () => SceneManager.LoadScene("House");
 		root.Q<Button>("Exit").clicked += () => Application.Quit();
+		
+		 var logo = root.Q<VisualElement>("LogoMove");
+        logo.RegisterCallback<TransitionEndEvent>(evt => logo.ToggleInClassList("rotate"));
+        root.schedule.Execute(() => logo.ToggleInClassList("rotate")).StartingIn(200);
+		
+		
 		
         
     }
