1aa64b50b2097493668d20034ecfc17b0421ce9f
Liz <lfalconriv@gmail.com>
2023-12-18 17:35:31 -0300

    creditos

--- Parada88/Assets/UI Toolkit/Pannels/MainMenuPresenter.cs
+++ Parada88/Assets/UI Toolkit/Pannels/MainMenuPresenter.cs
@@ -7,20 +7,52 @@ using UnityEngine.SceneManagement;
 public class MainMenuPresenter : MonoBehaviour
 {
 	
+	private VisualElement root;
+	private VisualElement mainMenuScreen;
+	private VisualElement creditsScreen;
+	
     void Start()
     {
-		var root = GetComponent<UIDocument>().rootVisualElement;
+		root = GetComponent<UIDocument>().rootVisualElement;
+		
+		mainMenuScreen = root.Q<VisualElement>("MainMenuScreen");
+		creditsScreen = root.Q<VisualElement>("CreditsScreen");
 	
 		
 		root.Q<Button>("Start").clicked += () => SceneManager.LoadScene("CutScene1");
-		root.Q<Button>("Exit").clicked += () => Application.Quit();
-		
-		 var logo = root.Q<VisualElement>("LogoMove");
-        logo.RegisterCallback<TransitionEndEvent>(evt => logo.ToggleInClassList("rotate"));
-        root.schedule.Execute(() => logo.ToggleInClassList("rotate")).StartingIn(200);
-		
-		
-		
+		root.Q<Button>("Exit").clicked += () => Application.Quit();		
+				
+			
+		setupLogoMove();		
+		setupCreditsScreen();		
         
     }
+	
+	void setupLogoMove(){
+		var logo = root.Q<VisualElement>("LogoMove");
+        logo.RegisterCallback<TransitionEndEvent>(evt => logo.ToggleInClassList("rotate"));
+        root.schedule.Execute(() => logo.ToggleInClassList("rotate")).StartingIn(200);
+	}
+	
+	void setupCreditsScreen(){
+		
+		var creditsText = root.Q<VisualElement>("CreditsText");
+        creditsText.RegisterCallback<TransitionEndEvent>(evt => {
+			creditsText.RemoveFromClassList("scroll");
+			creditsScreen.style.display = DisplayStyle.None;
+			mainMenuScreen.style.display = DisplayStyle.Flex;
+		});
+		
+		root.Q<Button>("Credits").clicked += () => {
+			mainMenuScreen.style.display = DisplayStyle.None;
+			creditsScreen.style.display = DisplayStyle.Flex;
+			
+			root.schedule.Execute(() => { creditsText.AddToClassList("scroll");}).StartingIn(200);
+		};			
+		
+		
+		
+		
+		
+	}
 }
