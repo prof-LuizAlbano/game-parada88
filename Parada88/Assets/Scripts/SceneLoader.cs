98dc822e18c70e198113b74aba921ac7873386bf
Liz <lfalconriv@gmail.com>
2023-12-20 18:11:22 -0300

    bake da luzes

--- Parada88/Assets/Scripts/SceneLoader.cs
+++ Parada88/Assets/Scripts/SceneLoader.cs
@@ -10,10 +10,18 @@ public class SceneLoader : MonoBehaviour
 
     [SerializeField]
 	 public int timeline = 0;
+
+    [SerializeField]
+	 public bool resetCursor = false;
  
 	 
     void Awake()
     {
+        if (resetCursor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
         TimelinesController.currentTimeline = timeline;
         SceneManager.LoadScene(scenename);
     }
